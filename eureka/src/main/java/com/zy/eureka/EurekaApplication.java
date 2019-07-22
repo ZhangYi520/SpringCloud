a63f2d45bb27eb1a451f514cc0529913a368c6ef
ZhangYi520 <1073988580@qq.com>
2019-07-22 16:41:28 +0800

    草

--- eureka/src/main/java/com/zy/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/zy/eureka/EurekaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 草拟吗，代码结构终于改完了，狗日的
+ */
 @SpringBootApplication //spring-boot 启动注解
 @EnableEurekaServer // spring-cloud 服务注解
 //@EnableEurekaClient
