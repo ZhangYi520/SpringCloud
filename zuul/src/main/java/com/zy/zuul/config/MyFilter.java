cae0cdca4e6be4c06ceecdd415d20e25883210a0
ZhangYi520 <1073988580@qq.com>
2019-07-23 11:46:17 +0800

    代码更新:分布式配置更新

--- zuul/src/main/java/com/zy/zuul/config/MyFilter.java
+++ zuul/src/main/java/com/zy/zuul/config/MyFilter.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Component
+//@Component
 public class MyFilter extends ZuulFilter {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
