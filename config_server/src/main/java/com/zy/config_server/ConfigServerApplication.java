55c0ca64a6e7b619c490f9d7f74ab986d3dbb337
ZhangYi520 <zzzyyy000>
2019-08-19 09:18:39 +0800

    cloud数据总线完成

--- config_server/src/main/java/com/zy/config_server/ConfigServerApplication.java
+++ config_server/src/main/java/com/zy/config_server/ConfigServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-@EnableEurekaClient
+@EnableConfigServer//配置中心服务端，标志该服务是config服务端
+@EnableEurekaClient//eureka客户端，用于注册服务
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
--- zy_filesystem/src/main/java/com/zy/zy_filesystem/no/UJpa.java
+++ zy_filesystem/src/main/java/com/zy/zy_filesystem/no/UJpa.java
@@ -1,12 +1,16 @@
 package com.zy.zy_filesystem.no;
 
-import javax.annotation.Generated;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
 
 @Entity
 @Table(name="u")
@@ -31,3 +35,95 @@ public class UJpa {
 	
 	
 }
+
+
+
+class demo{
+	public static void main(String[] args){
+			Scanner sc=new Scanner(System.in);
+			System.out.print("输入矩形宽度:");
+			//宽度
+			int i=sc.nextInt();
+			int j=9;
+			int zj=0;
+			boolean flag=false;
+			if(i%2==0){//偶数
+				i++;
+				zj=i/2;
+				flag=true;
+			}else{//奇数
+				zj=i/2;
+			}
+		Object [][] arr=new Object[i][i];//存储数据的
+//		System.out.println("中心点为:（"+zj+","+zj+")");
+		int zjz=j-zj;
+//		System.out.println("中心点值:"+zjz);
+		arr[zj][zj]=zjz;
+		//纵
+		for (int a = 0; a < i; a++) {
+			//横
+			for (int b = 0; b < i; b++) {
+				arr[a][b]=Math.abs(zj-a) - Math.abs(zj-b) >0 ? zjz+Math.abs(zj-a):zjz+Math.abs(zj-b);
+			}
+		}
+
+		if(flag){
+			//纵
+			for (int a = 0; a < i; a++) {
+				//横
+				for (int b = 0; b < i; b++) {
+					if(a==zj || b==zj){
+						arr[a][b]=null;
+					}
+				}
+			}
+		}
+
+		//偶数特殊处理
+		for(Object [] temp:arr)
+		{
+			for(Object a:temp)
+			{
+				if(a!=null){
+					System.out.print(a);
+				}
+			}
+//			if(temp)
+			System.out.println();
+		}
+	}
+}
+
+enum meiju{
+	a,b,c,d
+}
+
+class User{
+	private String name;
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
