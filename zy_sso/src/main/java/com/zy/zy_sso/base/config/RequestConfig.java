56a13c0bda177b8caf71e13cb01bbfbd51938440
ZhangYi520 <zzzyyy000>
2019-09-19 15:26:02 +0800

    https

---
+++ zy_sso/src/main/java/com/zy/zy_sso/base/config/RequestConfig.java
@@ -0,0 +1,54 @@
+package com.zy.zy_sso.base.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: SpringCloud
+ * @description: https转发
+ * @author: zhang yi
+ * @create: 2019-09-19 15:13
+ */
+
+@Configuration
+public class RequestConfig {
+
+//    @Value("server.port")
+//    private String HTTP_PORT;
+
+    @Bean
+    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+        return factory;
+    }
+
+    private Connector createTomcatConnector() {
+        Connector connector = new
+                Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        // Http 的请求端口为 8081
+        connector.setPort(8081);
+        connector.setSecure(false);
+        // https 的端口为 8080
+        connector.setRedirectPort(8082);
+        return connector;
+    }
+
+}
---
+++ zy_sso/src/main/java/com/zy/zy_sso/base/quartz/HelloJob.java
@@ -0,0 +1,20 @@
+package com.zy.zy_sso.base.quartz;
+
+
+
+/**
+ * @program: SpringCloud
+ * @description: quartzDemo
+ * @author: zhang yi
+ * @create: 2019-09-02 15:48
+ */
+public class HelloJob {
+//    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        //打印当前的执行时间 例如 2017-11-23 00:00:00
+//        Date date = new Date();
+//        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        System.out.println("现在的时间是："+ sf.format(date));
+//        //具体的业务逻辑
+//        System.out.println("Hello Quartz");
+//    }
+}
