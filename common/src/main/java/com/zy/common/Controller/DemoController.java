c1f0540b84e7757494beb051fcc295cb759f8961
ZhangYi520 <1073988580@qq.com>
2020-03-06 17:40:14 +0800

    配置中心文件

--- common/src/main/java/com/zy/common/Controller/DemoController.java
+++ common/src/main/java/com/zy/common/Controller/DemoController.java
@@ -2,6 +2,7 @@ package com.zy.common.Controller;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.Serializable;
 
 @RestController
+@RefreshScope
 public class DemoController {
 
     @Value("${server.port}")
     String port;
+
+    @Value("${foo}")
+    String foo;
 
     @GetMapping("/demo")
     public User demo(@RequestParam(value = "name", defaultValue = "小毅毅") String name) {
@@ -20,6 +25,11 @@ public class DemoController {
         u.setName(name);
         u.setAge(Integer.valueOf(port));
         return u;
+    }
+
+    @GetMapping("/hi")
+    public String hi1() {
+        return foo;
     }
 }
 
