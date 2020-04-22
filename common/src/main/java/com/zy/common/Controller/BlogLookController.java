82fbb32684ef23b7d237baf20b6f858799a9afd7
ZhangYi520 <zzzyyy000>
2020-04-22 16:50:40 +0800

    部署修改

--- common/src/main/java/com/zy/common/Controller/BlogLookController.java
+++ common/src/main/java/com/zy/common/Controller/BlogLookController.java
@@ -3,6 +3,7 @@ package com.zy.common.controller;
 import com.zy.common.Service.impl.BlogLookServiceImpl;
 import com.zy.common.base.util.ReturnResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2020-04-21 16:30
  */
 @RestController
-@RequestMapping("blogLook")
+@RequestMapping("/blogLook")
 public class BlogLookController {
 
     @Autowired
     private BlogLookServiceImpl blogLookServiceImpl;
+
+    @GetMapping("/get")
     public ReturnResult blogLook(@RequestParam(value = "id",required = true) String id){
-        return ReturnResult.ok(blogLookServiceImpl.blogLook(id));
+        return blogLookServiceImpl.blogLook(id);
     }
 }
