09b3aa32c6481a9a776601659f48e0b1050b1960
ZhangYi520 <zzzyyy000>
2020-05-11 17:25:28 +0800

    优化

--- zuul/src/main/java/com/zy/zuul/config/MyFilter.java
+++ zuul/src/main/java/com/zy/zuul/config/MyFilter.java
@@ -5,12 +5,14 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.zy.zuul.base.utils.ReturnResult;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
@@ -32,6 +34,7 @@ public class MyFilter extends ZuulFilter {
      * 放行地址
      */
     private List<String> paths;
+
     public MyFilter() {
         super();
         paths = new ArrayList<>();
@@ -78,7 +81,7 @@ public class MyFilter extends ZuulFilter {
         PathMatcher matcher = new AntPathMatcher();
         /**匹配当前路径是否在放行地址中，
          * 如果在，则最后optional有数据，optional.isPresent()==true,就不用拦截*/
-        Optional<String> optional =paths.stream().filter(t->matcher.match(t,uri)).findFirst();
+        Optional<String> optional = paths.stream().filter(t -> matcher.match(t, uri)).findFirst();
         return !optional.isPresent();
     }
 
@@ -87,9 +90,9 @@ public class MyFilter extends ZuulFilter {
         System.out.println(matcher.match("/login/**", "/xxx/login"));
 
     }
+
     /**
      * 过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
-     *
      *
      * @return
      * @throws ZuulException
@@ -101,28 +104,49 @@ public class MyFilter extends ZuulFilter {
         HttpServletRequest request = ctx.getRequest();
 
         log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURI()));
-        Object accessToken = request.getHeader("accessToken");
+        String accessToken = request.getHeader("accessToken");
 //        accessToken=null;
-        if (accessToken == null || "".equals(accessToken) || "null".equals(accessToken)) {
+//        if (accessToken == null || "".equals(accessToken) || "null".equals(accessToken)) {
+          if (StringUtils.isBlank(accessToken)) {
             log.warn("token is empty");
             try {
-//            HttpServletResponse response = ctx.getResponse();
-//            response.reset();
-            ctx.setSendZuulResponse(false);
-            ctx.setResponseStatusCode(200);
-            ctx.getResponse().setHeader("Content-type", "text/html;charset=UTF-8");
-//            response.getWriter().write(JSON.toJSONString(ReturnResult.build(401, "未授权")));
-          ctx.getResponse().getWriter().write(JSON.toJSONString(ReturnResult.build(401, "未授权")));
-//                ctx.getResponse().getWriter().write("未授权");
+                ctx.setSendZuulResponse(false);
+                ctx.setResponseStatusCode(200);
+                ctx.getResponse().setHeader("Content-type", "text/html;charset=UTF-8");
+                ctx.getResponse().getWriter().write(JSON.toJSONString(ReturnResult.build(401, "未授权")));
 
             } catch (Exception e) {
             }
             return null;
-//            return ReturnResult.build(401, "未授权");
-        }else{
-            log.info("token:"+accessToken);
-            return null;
         }
 
+//        Integer userId = (Integer)redisTemplate.opsForValue().get(token);
+//        if(userId!=null){
+//            redisTemplate.expire(token, 60 * 60 * 2, TimeUnit.SECONDS);
+//            setCookie(response, token);
+//            return true;
+//        }else{
+//            redisTemplate.delete(token);
+//            response.setHeader("Content-type", "text/html;charset=UTF-8");
+//            response.getWriter().write(JsonUtils.marshalToString(ReturnResult.build(401, "登录信息已过期")));
+//            return false;
+//        }
+
+        return null;
+    }
+
+
+    /**
+     * 写入客服端cookie
+     * @param response
+     * @param token
+     */
+    private void setCookie(HttpServletResponse response, String token){
+        // 生成cookie
+        Cookie cookie = new Cookie("USER_TOKEN", token);
+        cookie.setPath("/");
+        //(24*60*60); 一天
+        cookie.setMaxAge(60 * 60 * 2);
+        response.addCookie(cookie);
     }
 }
